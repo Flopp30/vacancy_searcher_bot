cc07480a9ee5a1050adad22d06ecc728cc6690aa
Artem Pospeev <pospeev.artem@icloud.com>
2023-06-22 21:15:37 +0300

    changed templates

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from app.endpoints.routers import main_router
 
-
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(main_router)
